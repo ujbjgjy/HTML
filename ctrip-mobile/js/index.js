d16de4ae9077d9e353fab99c9bd6c452278810a4
ujbj <ujbj@jtww1003.onexmail.com>
2023-01-13 23:24:19 +0800

    2023-1-13 23:09

--- ctrip-mobile/js/index.js
+++ ctrip-mobile/js/index.js
@@ -83,26 +83,4 @@ window.addEventListener('load', function () {
     goBack.addEventListener('click', function () {
         window.scroll(0, 0);
     });
-    //封装tap,解决click300ms延时
-    function tap(obj, callback) {
-        var isMove = false;
-        var startTime = 0;//记录触摸时候的时间变量
-        obj.addEventListener('touchstart', function (e) {
-            startTime = Date.now();//记录触摸时间
-        });
-        obj.addEventListener('touchmove', function (e) {
-            isMove = true;//看看是否有滑动，有滑动算拖拽，不算点击
-        });
-        obj.addEventListener('touchend', function (e) {
-            if (!isMove && (Date.now() - startTime) < 150) {//如果手指触摸和离开时间小于150ms算点击
-                ca11back && ca11back();//执行回调函数
-                isMove = false;//取反重置
-                startTime = 0;
-            }
-        });
-        //调用
-        tap(div, function () {
-            //执行代码
-        });
-    }
 })
