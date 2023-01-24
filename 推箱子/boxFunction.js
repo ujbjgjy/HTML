01a25eb5004b85db76034df7477254b80d9e999b
ujbj <ujbj@jtww1003.onexmail.com>
2023-01-24 22:04:26 +0800

    2023-1-23 23:01

--- 推箱子/boxFunction.js
+++ 推箱子/boxFunction.js
@@ -3,6 +3,7 @@ var col = 10;
 var row = 10;
 // 创建地图
 function createMap(map, info, arrMap) {
+    info.boxNum = 0;
     for (var i = 0; i < col; i++) {
         for (var j = 0; j < row; j++) {
             var div = document.createElement('div');
@@ -14,19 +15,17 @@ function createMap(map, info, arrMap) {
                     break;
                 case info.wall:
                     div.className = 'wall';
-                    div.innerHTML = '墙';
                     break;
                 case info.target:
                     div.className = 'target';
-                    div.innerHTML = '目标';
                     break;
                 case info.box:
                     div.className = 'box';
                     div.innerHTML = '箱子';
+                    info.boxNum++;
                     break;
                 case info.fig:
                     div.className = 'fig';
-                    div.innerHTML = '人物';
                     info.x = i;
                     info.y = j;
                     break;
@@ -36,7 +35,6 @@ function createMap(map, info, arrMap) {
                     break;
                 case info.figTarget:
                     div.className = 'figTarget';
-                    div.innerHTML = 'f和t';
                     info.x = i;
                     info.y = j;
                     break;
@@ -53,8 +51,7 @@ function getMapInfo(map, arrMap) {
     var arrEle = map.querySelectorAll('div');
     for (var i = 0; i < col; i++) {
         for (var j = 0; j < row; j++) {
-            var tmp = i * row + j;
-            arrMap[i][j] = parseInt(arrEle[tmp].getAttribute('index'));
+            arrMap[i][j] = parseInt(arrEle[i * row + j].getAttribute('index'));
         }
     }
 }
--- 推箱子/main.js
+++ 推箱子/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function () {
     var map = document.querySelector('.map'); // 获取地图
+    var nextLevel = document.querySelector('.Next-level-bg');
     var info = {
         cle: 0, // 空地
         wall: 1, // 墙
@@ -8,69 +9,99 @@ window.addEventListener('load', function () {
         fig: 4, // 人物
         boxTarget: 2 + 3,
         figTarget: 2 + 4,
+        boxNum: 0,
         x: 0,
         y: 0
     };
-    console.log(info.figTarget);
     var arrMap = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 2, 0, 0, 0, 0, 0, 0, 2, 1],
         [1, 0, 3, 0, 0, 0, 0, 3, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 2, 3, 2, 2, 6, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 4, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 3, 0, 0, 0, 0, 3, 0, 1],
+        [1, 0, 3, 0, 0, 0, 0, 0, 3, 1],
         [1, 2, 0, 0, 0, 0, 0, 0, 2, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
     // 创建地图
     createMap(map, info, arrMap);
-    console.log(info.x, info.y);
     // 获取地图信息
     getMapInfo(map, arrMap);
-    document.body.addEventListener('keydown', function (e) {
+    function operation(e) {
         if (e.key == 'ArrowLeft') {
-            // 前面是空地 移动
-            if (arrMap[info.x][info.y - 1] == info.cle) {
-                arrMap[info.x][info.y ] -= info.fig;
+            // 前面是空地 或者 目标
+            if (arrMap[info.x][info.y - 1] == info.cle || arrMap[info.x][info.y - 1] == info.target) {
+                arrMap[info.x][info.y] -= info.fig;
                 arrMap[info.x][info.y - 1] += info.fig;
             }
-            // 前面是目标
-            else if (arrMap[info.x][info.y - 1] == info.target) {
-                arrMap[info.x][info.y ] -= info.fig;
-                arrMap[info.x][info.y - 1] += info.fig;
-            }
-            // 前面是箱子
-            else if (arrMap[info.x][info.y - 1] == info.box) {
-                // 箱子前面是空地
-                if (arrMap[info.x][info.y - 2] == info.cle) {
-                    arrMap[info.x][info.y] -= info.fig;
-                    arrMap[info.x][info.y - 1] += info.fig - info.box;
-                    arrMap[info.x][info.y - 2] = info.box;  
-                }
-                // 箱子前是目标的话
-                else if (arrMap[info.x][info.y - 2] == info.target) {
+            // 前面是箱子 或者 箱子 + 目标
+            else if (arrMap[info.x][info.y - 1] == info.box || arrMap[info.x][info.y - 1] == info.boxTarget) {
+                // 箱子前面是空地 和 箱子+目标
+                if (arrMap[info.x][info.y - 2] == info.cle || arrMap[info.x][info.y - 2] == info.target) {
                     arrMap[info.x][info.y] -= info.fig;
                     arrMap[info.x][info.y - 1] += info.fig - info.box;
                     arrMap[info.x][info.y - 2] += info.box;
-                } 
+                }
             }
         }
         else if (e.key == 'ArrowRight') {
-            arrMap[info.x][info.y ] = info.cle;
-            arrMap[info.x][info.y + 1] = info.fig;
+            // 前面是空地 或者 目标
+            if (arrMap[info.x][info.y + 1] == info.cle || arrMap[info.x][info.y + 1] == info.target) {
+                arrMap[info.x][info.y] -= info.fig;
+                arrMap[info.x][info.y + 1] += info.fig;
+            }
+            // 前面是箱子 或者 箱子 + 目标
+            else if (arrMap[info.x][info.y + 1] == info.box || arrMap[info.x][info.y + 1] == info.boxTarget) {
+                // 箱子前面是空地 和 箱子+目标
+                if (arrMap[info.x][info.y + 2] == info.cle || arrMap[info.x][info.y + 2] == info.target) {
+                    arrMap[info.x][info.y] -= info.fig;
+                    arrMap[info.x][info.y + 1] += info.fig - info.box;
+                    arrMap[info.x][info.y + 2] += info.box;
+                }
+            }
         }
         else if (e.key == 'ArrowUp') {
-            arrMap[info.x][info.y ] = info.cle;
-            arrMap[info.x - 1][info.y] = info.fig;
+            // 前面是空地 或者 目标
+            if (arrMap[info.x - 1][info.y] == info.cle || arrMap[info.x - 1][info.y] == info.target) {
+                arrMap[info.x][info.y] -= info.fig;
+                arrMap[info.x - 1][info.y] += info.fig;
+            }
+            // 前面是箱子 或者 箱子 + 目标
+            else if (arrMap[info.x - 1][info.y] == info.box || arrMap[info.x - 1][info.y] == info.boxTarget) {
+                // 箱子前面是空地 和 箱子+目标
+                if (arrMap[info.x - 2][info.y] == info.cle || arrMap[info.x - 2][info.y] == info.target) {
+                    arrMap[info.x][info.y] -= info.fig;
+                    arrMap[info.x - 1][info.y] += info.fig - info.box;
+                    arrMap[info.x - 2][info.y] += info.box;
+                }
+            }
         }
         else if (e.key == 'ArrowDown') {
-            arrMap[info.x][info.y ] = info.cle;
-            arrMap[info.x + 1][info.y] = info.fig;
+            // 前面是空地 或者 目标
+            if (arrMap[info.x + 1][info.y] == info.cle || arrMap[info.x + 1][info.y] == info.target) {
+                arrMap[info.x][info.y] -= info.fig;
+                arrMap[info.x + 1][info.y] += info.fig;
+            }
+            // 前面是箱子 或者 箱子 + 目标
+            else if (arrMap[info.x + 1][info.y] == info.box || arrMap[info.x + 1][info.y] == info.boxTarget) {
+                // 箱子前面是空地 和 箱子+目标
+                if (arrMap[info.x + 2][info.y] == info.cle || arrMap[info.x + 2][info.y] == info.target) {
+                    arrMap[info.x][info.y] -= info.fig;
+                    arrMap[info.x + 1][info.y] += info.fig - info.box;
+                    arrMap[info.x + 2][info.y] += info.box;
+                }
+            }
         }
+        // 清空map里的数据
         map.innerHTML = '';
+        // 重新创建地图
         createMap(map, info, arrMap);
-        getMapInfo(map, arrMap);
-    });
+        if (info.boxNum == 0) {
+            nextLevel.style.display = 'block';
+            document.body.removeEventListener('keydown', operation);
+        }
+    }
+    document.body.addEventListener('keydown', operation);
 })
