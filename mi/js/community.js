6f3947c3df096e95751404bec1b038e0252fe058
ujbj <ujbj@jtww1003.onexmail.com>
2023-02-05 19:27:05 +0800

    2023-1-31 11:22

---
+++ mi/js/community.js
@@ -0,0 +1,63 @@
+window.addEventListener('load', function () {
+    function animate(obj, target, callback) {
+        // 只保留一个定时器
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function () {
+            // 结束条件
+            if (obj.offsetLeft == target) {
+                clearInterval(obj.timer);
+                // 回调函数
+                callback && callback();
+            }
+            // 把剩余的距离分十份做步长
+            var step = (target - obj.offsetLeft) / 10;
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            obj.style.left = obj.offsetLeft + step + 'px';
+        }, 15);
+    }
+    var wrap = document.querySelector('.main-poster-wrap');
+    var poster = document.querySelector('.main-poster');
+    var wrapWidth = wrap.offsetWidth;
+    var num = 0;
+    // 生成小圆点
+    var dots = document.querySelector('.main-poster-dot');
+    for (var i = 0; i < poster.children.length; i++) {
+        var dot = document.createElement('li');
+        dot.className = 'dot';
+        dot.setAttribute('index', i);
+        dots.appendChild(dot);
+    }
+    dots.children[0].classList.add('current');
+    // 点击小圆点
+    for (var i = 0; i < dots.children.length; i++) {
+        dots.children[i].addEventListener('click', function () {
+            for (var j = 0; j < dots.children.length; j++) {
+                dots.children[j].classList.remove('current');
+            }
+            this.classList.add('current');
+            num = this.getAttribute("index");
+            animate(poster, -num * wrapWidth);
+            console.log(poster.style.left, num);
+        });
+    }
+    // 轮播图
+    var first = poster.children[0].cloneNode(true);
+    poster.appendChild(first);
+    function rotation() {
+        // 移动到最后一张(也就是克隆的第一图片)，来瞬间切到第一张
+        if (num == poster.children.length - 1) {
+            num = 0;
+            poster.style.left = 0;
+        }
+        num++;
+        animate(poster, -num * wrapWidth);
+    }
+    var timer = setInterval(rotation, 5000);
+    // 鼠标悬停时停止轮播图
+    wrap.addEventListener('mouseenter', function () {
+        clearInterval(timer);
+    });
+    wrap.addEventListener('mouseleave', function () {
+        timer = setInterval(rotation, 5000);
+    });
+})
