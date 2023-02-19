6a395bac8ca5f0a82ec6c18b8c2598b7ad2b6c3c
ujbjgjy <ujbj@jtww1003.onexmail.com>
2023-02-19 15:44:27 +0800

    2-19

--- mi/js/service.js
+++ mi/js/service.js
@@ -1,69 +1,90 @@
+// 动画函数
+function animate(obj, target, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        // 终止条件
+        if (obj.offsetLeft == target) {
+            // 停止动画
+            clearInterval(obj.timer);
+            // 回调函数
+            callback && callback();
+        }
+        // 剩余路长分为十份
+        var step = (target - obj.offsetLeft) / 10;
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        obj.style.left = obj.offsetLeft + step + 'px';
+    }, 15);
+}
+//排他思想
+function exclusive(dots) {
+    for (var i = 0; i < dots.children.length; i++) {
+        dots.children[i].classList.remove('yuan_bg');
+    }
+}
+
 window.addEventListener('load', function () {
-    function animaTion(obj, target, callback) {
-        // 清除之前调用函数时的定时器
-        clearInterval(obj.timer);
-        obj.timer = setInterval(function () {
-            // 得到步长：(目标距离 - 当前距离) / 10 
-            //  做缓速的效果 就是步长再不断变小
-            var step = (target - obj.offsetLeft) / 10;
-            // 如果是正数取大，如果负数取小
-            step = step > 0 ? Math.ceil(step) : Math.floor(step);
-            // 如果到了指定的距离停止定时器        
-            if (obj.offsetLeft == target) {
-                // 停止定时器
-                clearInterval(obj.timer);
-                // 执行回调函数
-                //  如果参有函数调用
-                if (callback) {
-                    callback();
+    // 关闭通知
+    (function closeNotify() {
+        var notify = document.querySelector('.notify');
+        var close = document.querySelector('.close-button');
+        close.onclick = function () {
+            notify.style.display = 'none';
+        }
+    }());
+
+    // 轮播图
+    (function rotation() {
+        // 获取元素
+        var focus = document.querySelector('.promotion'); // 焦点图
+        var imgs = document.querySelector('.container');
+        var dots = document.querySelector('.promotion_dot'); // 小点
+        // 动态生成小点
+        for (var i = 0; i < imgs.children.length; i++) {
+            // 只是创建了元素，但是还得指添加的地方
+            var dot = document.createElement('div');
+            dot.setAttribute('index', i);
+            dots.appendChild(dot);
+            dot.addEventListener('click', function () {
+                exclusive(dots);
+                this.classList.add('yuan_bg');
+                num = this.getAttribute('index');
+                animate(imgs, -num * focusWidth);
+            });
+        }
+        dots.children[0].classList.add('yuan_bg');
+        // 播放图片
+        var num = 0; // 通过num来播放图片
+        var focusWidth = focus.offsetWidth;
+        var firstImg = imgs.children[0].cloneNode(true);
+        imgs.appendChild(firstImg);
+        var playImg = setInterval(function () {
+            if (num == imgs.children.length - 1) {
+                num = 0;
+                focus.offsetLeft = 0;
+            }
+            num++;
+            animate(imgs, -num * focusWidth);
+            exclusive(dots);
+            dots.children[num % dots.children.length].classList.add('yuan_bg');
+        }, 10000);
+    }());
+
+    // 底部模块
+    (function footer() {
+        var tab_list = document.querySelector('.wenti_tab_list');
+        var itme = document.querySelectorAll('.wenti_tab_itme');
+        for (var i = 0; i < tab_list.children.length; i++) {
+            tab_list.children[i].setAttribute('index', i);
+            tab_list.children[i].onmouseover = function () {
+                for (var i = 0; i < tab_list.children.length; i++) {
+                    tab_list.children[i].classList.remove('wenti_current');
                 }
+                this.classList.add('wenti_current');
+                for (var i = 0; i < itme.length; i++) {
+                    itme[i].style.display = 'none';
+                }
+                itme[this.getAttribute('index')].style.display = 'block';
             }
-            // 移动盒子：当前位置 + 步长
-            obj.style.left = obj.offsetLeft + step + 'px';
-        }, 15);
-    };
-    var focus = document.querySelector('.promotion');
-    var container = focus.querySelector('.container');
-    var dot = focus.querySelector('.promotion_dot');
-    console.log(dot);
-    var i = 0;
-    // 自定义属性
-    // 动态生成圆点
-    for (i = 0; i < container.children.length; i++) {
-        var div = document.createElement('div');
-        div.setAttribute('index', i);
-        dot.appendChild(div);
-    }
-    dot.children[0].className = 'yuan_bg';
-    var first = container.children[0].cloneNode(true);
-    container.appendChild(first);
-    var num = 0;
-    for (i = 0; i < dot.children.length; i++) {
-        dot.children[i].addEventListener('click', function () {
-            for (var j = 0; j < dot.children.length; j++) {
-                dot.children[j].className = '';
-            }
-            this.className = 'yuan_bg';
-            num = this.getAttribute('index');
-            animaTion(container, -num * focus.offsetWidth);
-            console.log(num);
-        });
-    }
-    this.setInterval(function () {
-        if (num == container.children.length - 1) {
-            num = 0;
-            container.style.left = 0;
         }
-        for (i = 0; i < dot.children.length; i++) {
-            dot.children[i].className = '';
-        }
-        if (num == dot.children.length) {
-            dot.children[num - 1].className = 'yuan_bg';
-        }
-        else {
-            dot.children[num].className = 'yuan_bg';
-        }
-        num++;
-        animaTion(container, -num * focus.offsetWidth);
-    }, 5000);
-})
+    }());
+});
