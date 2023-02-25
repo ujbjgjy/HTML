bfaa43dc0cf2dad2df53af3a8364055bbaff522d
ujbjgjy <ujbj@jtww1003.onexmail.com>
2023-02-25 23:25:39 +0800

    2-25

--- 打字游戏/js/index.js
+++ 打字游戏/js/index.js
@@ -1,8 +1,8 @@
-// 随机生成数字
+// 获取随机数字
 function getRand(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-// 生成随机字符
+// 获取随机字符
 function getStr() {
     var arrStr = [];
     var arrRand = [];
@@ -11,17 +11,46 @@ function getStr() {
         arrStr.push(String.fromCharCode(i));
     }
     // 生成随机数
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < getRand(3, 7); i++) {
         // 0~25之间
         var index = getRand(0, 26);
         arrRand.push(arrStr[index]);
     }
-    return arrRand;
+    return arrRand.join('');
+}
+// 获取多个随机坐标
+function coordinate() {
+    var mapWidth = map.clientWidth;
+    var arr = new Array(3);
+    var i = 0;
+    while (i < arr.length) {
+        // 找不到赋值
+        var x = getRand(0, mapWidth - 50);
+        var y = getRand(50, 200);
+        if (arr.indexOf(x) == -1) {
+            // arr[i].x = x;
+            // arr[i].y = y;
+            i++;
+        }
+    }
+    return arr;
+}
+// 生成文字
+function createText() {
+    var arrText = coordinate();
+    for (var i = 0; i < arrText.length; i++) {
+        var text = document.createElement('div');
+        text.className = 'text';
+        text.style.left = arrText[i] + 'px';
+        text.innerHTML = getStr();
+        map.appendChild(text);
+    }
 }
 
 
-
-
 window.addEventListener('load', function () {
-    var str = getStr();
+    var map = document.querySelector('.map');
+    console.log(coordinate());
+    createText();
+    console.log(getStr());
 });
